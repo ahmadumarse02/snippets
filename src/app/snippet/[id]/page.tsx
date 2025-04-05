16d60720dd3b298e8e3fc4e69910e71407930fa6
ahmadumarse02 <ahmadumarse02@gmail.com>
2025-04-05 18:16:50 +0500

    snippets

--- src/app/snippet/[id]/page.tsx
+++ src/app/snippet/[id]/page.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 import * as actions from "@/actions";
 import { notFound } from "next/navigation";
 
+export const dynamic = 'force-static';
+
 const SnippetDetail = async ({
   params,
 }: {
