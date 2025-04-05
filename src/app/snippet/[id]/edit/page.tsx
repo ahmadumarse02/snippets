f777d12815b9e6e91275a2ba3c4b102b7f8f6ed0
ahmadumarse02 <ahmadumarse02@gmail.com>
2025-04-05 18:18:17 +0500

    snippets

--- src/app/snippet/[id]/edit/page.tsx
+++ src/app/snippet/[id]/edit/page.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { prisma } from "@/lib/prisma";
 import EditorSnippet from "@/components/EditorSnippet";
 
+export const dynamic = 'force-static';
+
 const EditPage = async ({
   params,
 }: {
